84e32cf94d7211d73a53365a1a9265b560c5c87c
StevenGnt <stevengangnant@hotmail.com>
2018-03-12 21:52:04 +0100

    Added another background

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('copyright-year').textContent = (new Date()).getFullYear();
 
 // Random background
-  const nbBackgrounds = 1;
+  const nbBackgrounds = 2;
   const bodyClass = `background-${Math.floor(Math.random() * nbBackgrounds)}`;
   document.getElementsByTagName('body')[0].classList.add(bodyClass);
 });
